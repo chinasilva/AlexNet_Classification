ed1a511dfec78e33066361134884208a6c2fb8f6
YSP <YSP@email.com>
2019-06-04 18:18:30 +0800

    使用kaggle高分网络

--- src/main.py
+++ src/main.py
@@ -5,31 +5,31 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 import time
-from src.AlexNet import AlexNet
+from src.MyNet import MyNet
 
 def main():
 
 	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-	epochs = 100
+	epochs = 1
 	batch_size = 64
 
-	net = AlexNet(10).to(device)
+	net = MyNet(10).to(device)
 	criterion = nn.CrossEntropyLoss()
 	optimizer = optim.Adam(net.parameters(), weight_decay = 0, amsgrad = False, lr = 0.001, betas = (0.9, 0.999), eps = 1e-08)
 
 	transform_train = transforms.Compose([
-	        transforms.Resize(32),
-	        transforms.CenterCrop(32),
+	        # transforms.Resize(32),
+	        # transforms.CenterCrop(32),
 	        transforms.ToTensor(),
-	        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+	        # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 	    ])
-	dataset = datasets.CIFAR10("datasets/", train=True, download=True,transform=transform_train)
+	dataset = datasets.CIFAR10("datasets/", train=True, download=False,transform=transform_train)
 	dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
 	transform_test = transforms.Compose([
 	        transforms.ToTensor(),
-	        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+	        # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 	    ])
 	testdataset = datasets.CIFAR10("datasets/", train=False, download=True,transform=transform_test)
 	testdataloader = torch.utils.data.DataLoader(testdataset, batch_size=batch_size, shuffle=False)
@@ -70,7 +70,8 @@ def main():
 				correct += (predicted == target).sum()
 				accuracy = correct.float() / total
 			print("[epochs - {0}]Accuracy:{1}%".format(i + 1, (100 * accuracy)))
-		# torch.save(net, "models/net.pth")
-	torch.save(model_object.state_dict(), 'AlexNet_Classification/models/net.pkl')
+	torch.save(net, "models/net.pth")
+	# torch.save(model_object.state_dict(), 'AlexNet_Classification/models/net.pkl')
+	# torch.save(model_object.state_dict(), 'models/net.pkl')
 	# model_object.load_state_dict(torch.load('params.pkl'))
 
